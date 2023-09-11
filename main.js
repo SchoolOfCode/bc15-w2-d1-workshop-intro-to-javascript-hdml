5cc0dc450d1f04acf9ef2e5065b13639aa2ece47
Dave-deJong <dndj81@yahoo.co.uk>
2023-09-11 16:53:32 +0100

    Up to end of Ticket 3

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("Hello world!");
+let age = 25; 
+let name = "Lee"; 
+let isAdmin = false;
+console.log(typeof age); // number
+console.log(typeof name); // string 
+console.log(typeof isAdmin); // boolean
+console.log(7000 / 10 + 100 - 11);
+console.log(10 + 5 * 2)
+console.log(true || false && true)
+console.log(7 > 6); // true
+console.log(3 < 3); // false
+
+console.log(2 === 2); // true
+console.log('2' === 2); // false
+let favoriteFood = "apple"
